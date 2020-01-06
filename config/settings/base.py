91331707b2b5831e8cb862fe0ce28febda37bc40
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2020-01-06 01:20:25 -0600

    comment out staticfiles_finders

--- config/settings/base.py
+++ config/settings/base.py
@@ -152,10 +152,10 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
-STATICFILES_FINDERS = [
-    "django.contrib.staticfiles.finders.FileSystemFinder",
-    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
-]
+# STATICFILES_FINDERS = [
+#     "django.contrib.staticfiles.finders.FileSystemFinder",
+#     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+# ]
 
 # MEDIA
 # ------------------------------------------------------------------------------
